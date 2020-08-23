2f6196a603b3f455c1c6fa0e493ad0ede290b83f
Hanyu <8245snake@gmail.com>
2020-08-23 18:34:12 +0900

    スペースキーでチェックできるようにした

--- DocumentCollector/CheckTreeView.cs
+++ DocumentCollector/CheckTreeView.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DocumentCollector
 {
@@ -17,11 +18,52 @@ namespace DocumentCollector
 
         private void CheckBox_Click(object sender, RoutedEventArgs e)
         {
-            var checkBox = (CheckBox)sender;
-            var source = (CheckTreeSource)checkBox.DataContext;
+            updateCheckState((CheckBox)sender);
+        }
 
+
+        /// <summary>
+        /// チェックボックスのクリックイベントから呼ばれる想定
+        /// </summary>
+        /// <param name="checkBox"></param>
+        private void updateCheckState(CheckBox checkBox) {
+            CheckTreeSource source = (CheckTreeSource)checkBox.DataContext;
             source.UpdateChildStatus();
             source.UpdateParentStatus();
         }
+
+        /// <summary>
+        /// TreeViewのイベントから呼ばれる想定
+        /// </summary>
+        /// <param name="source"></param>
+        private void updateCheckState(CheckTreeSource source)
+        {
+            source.UpdateChildStatus();
+            source.UpdateParentStatus();
+        }
+
+        /// <summary>
+        /// チェックボックスのFocusを無効にしているのでTreeViewのボタン押下を見ることに
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckBox_KeyUp(object sender, KeyEventArgs e) {
+            if (e.Key != Key.Space) {
+                return;
+            }
+
+            CheckTreeView view = (CheckTreeView)sender;
+            if (view == null) {
+                return;
+            }
+            CheckTreeSource item = (CheckTreeSource)view.SelectedItem;
+            if (item == null)
+            {
+                return;
+            }
+            item.IsChecked = !item.IsChecked;
+            updateCheckState(item);
+        }
+
     }
 }
