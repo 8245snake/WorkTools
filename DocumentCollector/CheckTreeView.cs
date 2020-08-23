3c414c17c4700c918742aef91d8263d70c7e38c4
Hanyu <8245snake@gmail.com>
2020-08-23 17:53:35 +0900

    メイン画面作成 チェックボックス付きTreeViewを追加

---
+++ DocumentCollector/CheckTreeView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DocumentCollector
+{
+    public partial class CheckTreeView : TreeView
+    {
+        public CheckTreeView()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            var source = (CheckTreeSource)checkBox.DataContext;
+
+            source.UpdateChildStatus();
+            source.UpdateParentStatus();
+        }
+    }
+}
---
+++ DocumentCollector/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DocumentCollector
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<CheckTreeSource> TreeRoot { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TreeRoot = new ObservableCollection<CheckTreeSource>();
+            var item1 = new CheckTreeSource() { Text = "Item1", IsExpanded = true, IsChecked = false };
+            var item11 = new CheckTreeSource() { Text = "Item1-1", IsExpanded = true, IsChecked = false };
+            var item12 = new CheckTreeSource() { Text = "Item1-2", IsExpanded = true, IsChecked = false };
+            var item2 = new CheckTreeSource() { Text = "Item2", IsExpanded = false, IsChecked = false };
+            var item21 = new CheckTreeSource() { Text = "Item2-1", IsExpanded = true, IsChecked = false };
+            TreeRoot.Add(item1);
+            TreeRoot.Add(item2);
+            item1.Add(item11);
+            item1.Add(item12);
+            item2.Add(item21);
+
+            DataContext = this;
+        }
+    }
+}
