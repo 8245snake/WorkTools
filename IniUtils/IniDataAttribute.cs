95429f43b930f102b9ae396619768df34bd0c009
Hanyu <8245snake@gmail.com>
2020-10-18 21:51:43 +0900

    IniDataAttributeを追加

---
+++ IniUtils/IniDataAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IniUtils
+{
+    /// <summary>
+    /// INIファイルに紐付くプロパティの属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IniDataAttribute : System.Attribute
+    {
+        /// <summary>
+        /// DefaultValueに使用する設定値の型
+        /// </summary>
+        public enum DataType
+        {
+            /// <summary>
+            /// 単純な文字列型
+            /// </summary>
+            StringData,
+            /// <summary>
+            /// 改行付きの文字列型（\nを改行コードに置換する）
+            /// </summary>
+            StringDataWithCarriageReturn,
+            /// <summary>
+            /// カンマ区切り型（配列にパースする）
+            /// </summary>
+            StringArrayData,
+            /// <summary>
+            /// 整数値型
+            /// </summary>
+            IntegerData,
+            /// <summary>
+            /// 浮動小数点数型
+            /// </summary>
+            FloatData,
+            /// <summary>
+            /// Boolean型（1がtrueでそれ以外はfalse）
+            /// </summary>
+            BooleanFlag
+        }
+
+        /// <summary>
+        /// iniファイルのパス
+        /// </summary>
+        public string File { set; get; }
+        /// <summary>
+        /// セクション名
+        /// </summary>
+        public string Section { set; get; }
+        /// <summary>
+        /// キー名
+        /// </summary>
+        public string Key { set; get; }
+        /// <summary>
+        /// 設定値の型
+        /// </summary>
+        public DataType ValueType { set; get; }
+        /// <summary>
+        /// デフォルト値
+        /// </summary>
+        public object DefaultValue { set; get; }
+    }
+}
--- IniUtils/IniFileUtility.cs
+++ IniUtils/IniFileUtility.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,7 +43,7 @@ namespace IniUtils
         /// <returns>設定値</returns>
         public static string GetIniValue(string path, string section, string key, string defaultVal = "")
         {
-            StringBuilder sb = new StringBuilder(256);
+            StringBuilder sb = new StringBuilder(4096);
             GetPrivateProfileString(section, key, defaultVal, sb, sb.Capacity, path);
             return sb.ToString();
         }
@@ -56,6 +58,138 @@ namespace IniUtils
         public static void SetIniValue(string path, string section, string key, string value)
         {
             WritePrivateProfileString(section, key, value, path);
+        }
+
+
+        /// <summary>
+        /// プロパティの属性で指定したiniを読んで結果を返す
+        /// </summary>
+        /// <param name="propertyName">プロパティ名</param>
+        /// <returns>設定値</returns>
+        public static object ReadToProperty(Type type, string propertyName)
+        {
+            foreach (PropertyInfo propertyInfo in type.GetProperties())
+            {
+                if (propertyInfo.Name != propertyName) { continue; }
+
+                IniDataAttribute attribute = Attribute.GetCustomAttribute(propertyInfo, typeof(IniDataAttribute)) as IniDataAttribute;
+                if (attribute == null) { continue; }
+
+                string IniFilePath = attribute.File;
+                string section = attribute.Section;
+                string key = attribute.Key;
+                object defVal = attribute.DefaultValue;
+                IniDataAttribute.DataType dataType = attribute.ValueType;
+                switch (dataType)
+                {
+                    case IniDataAttribute.DataType.StringData:
+                        return GetIniValue(IniFilePath, section, key, defVal?.ToString());
+                    case IniDataAttribute.DataType.StringDataWithCarriageReturn:
+                        return GetIniValue(IniFilePath, section, key, defVal?.ToString()).Replace(@"\n", "\r\n");
+                    case IniDataAttribute.DataType.StringArrayData:
+                        return GetIniValue(IniFilePath, section, key, defVal?.ToString()).Split(',');
+                    case IniDataAttribute.DataType.IntegerData:
+                        return ReadIniDataNumeric<int>(IniFilePath, section, key, defVal);
+                    case IniDataAttribute.DataType.FloatData:
+                        return ReadIniDataNumeric<double>(IniFilePath, section, key, defVal);
+                    case IniDataAttribute.DataType.BooleanFlag:
+                        return (GetIniValue(IniFilePath, section, key, defVal?.ToString()) == "1");
+                    default:
+                        break;
+                }
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// プロパティの属性で指定したiniに書き込む
+        /// </summary>
+        /// <param name="propertyName">プロパティ名</param>
+        /// <returns>設定値</returns>
+        public static void WriteToProperty(Type type, string propertyName, object value)
+        {
+            foreach (PropertyInfo propertyInfo in type.GetProperties())
+            {
+                if (propertyInfo.Name != propertyName) { continue; }
+
+                IniDataAttribute attribute = Attribute.GetCustomAttribute(propertyInfo, typeof(IniDataAttribute)) as IniDataAttribute;
+                if (attribute == null) { continue; }
+
+                string IniFilePath = attribute.File;
+                string section = attribute.Section;
+                string key = attribute.Key;
+                object defVal = attribute.DefaultValue;
+                IniDataAttribute.DataType dataType = attribute.ValueType;
+                switch (dataType)
+                {
+                    case IniDataAttribute.DataType.IntegerData:
+                    case IniDataAttribute.DataType.FloatData:
+                    case IniDataAttribute.DataType.StringData:
+                        SetIniValue(IniFilePath, section, key, value?.ToString());
+                        break;
+                    case IniDataAttribute.DataType.StringDataWithCarriageReturn:
+                        SetIniValue(IniFilePath, section, key, value?.ToString().Replace("\r\n", @"\n"));
+                        break;
+                    case IniDataAttribute.DataType.StringArrayData:
+                        SetIniValue(IniFilePath, section, key, string.Join(",", (string[])value));
+                        break;
+                    case IniDataAttribute.DataType.BooleanFlag:
+                        SetIniValue(IniFilePath, section, key, ((bool)value) ? "1" : "0");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// TryPerseのジェネリック版
+        /// </summary>
+        /// <typeparam name="T">なんでもいい</typeparam>
+        /// <param name="input">変換元</param>
+        /// <param name="result">変換先</param>
+        /// <returns>成功したらtrue</returns>
+        public static bool TryParse<T>(string input, ref T result)
+        {
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    //ConvertFromString(string text)の戻りは object なので T型でキャストする
+                    result = (T)converter.ConvertFromString(input);
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// ini設定値を数値で返す
+        /// </summary>
+        /// <param name="section">セクション</param>
+        /// <param name="key">キー</param>
+        /// <param name="defaultVal">デフォルト値</param>
+        /// <returns>設定値</returns>
+        public static object ReadIniDataNumeric<T>(string IniFilePath, string section, string key, object defaultVal)
+        {
+            string iniVal = GetIniValue(IniFilePath, section, key, defaultVal?.ToString());
+            T result = default;
+            if (!TryParse(iniVal, ref result))
+            {
+                // パースに失敗したらデフォルト値をTにパースして渡す
+                T parsedDefaultVal = default;
+                if (!TryParse(defaultVal.ToString(), ref parsedDefaultVal))
+                {
+                    parsedDefaultVal = default;
+                }
+                result = parsedDefaultVal;
+            }
+            return result;
         }
     }
 }
