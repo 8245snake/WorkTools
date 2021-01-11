058245eb08d4989c576f4fb49f9ba2756a78a44a
shingo.hanyu <shingo.hanyu@fujifilm.com>
2021-01-11 09:54:50 +0900

    チケット取得処理

---
+++ svn-client/CommandOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+
+namespace svn_client
+{
+    public class CommandOptions
+    {
+
+        [Option('u', "uri", Required = false, HelpText = "リポジトリのURI(例：http://orangeright/svnroot/OR/branches/Custom/NIS/or_group/OR/BedSide/System/Prog)")]
+        public string Uri { get; set; }
+
+        [Option('l', "limit", Required = false, HelpText = "ログを取得する件数の上限", Default = 200)]
+        public int Limit { get; set; }
+    }
+}
--- svn-client/Program.cs
+++ svn-client/Program.cs
@@ -1,16 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 using SharpSvn;
 
 namespace svn_client
 {
     class Program
     {
+
+        private static string _URI;
+        private static int _Limit;
+
         static void Main(string[] args)
+        {
+            if (!ParseArgs(args))
+            {
+                Console.WriteLine("ログを取得するURIを入力してください");
+                _URI = Console.ReadLine();
+            }
+            if (String.IsNullOrEmpty(_URI)) { return; }
+            OutputSvnLog();
+            Console.WriteLine("終了しました");
+            Console.ReadKey();
+        }
+
+        private static bool ParseArgs(string[] args)
+        {
+            var result = CommandLine.Parser.Default.ParseArguments<CommandOptions>(args);
+            if (result.Tag != ParserResultType.Parsed)
+            {
+                // コマンドの解析に失敗したり、不明なオプションがあったりHelp押されたりしたとき
+                return false;
+            }
+
+            var parsed = (Parsed<CommandOptions>)result;
+            _URI = parsed.Value.Uri;
+            _Limit = parsed.Value.Limit;
+
+            if (String.IsNullOrEmpty(_URI)) { return false; }
+            return true;
+        }
+
+        private static void OutputSvnLog()
+        {
+            using (SvnClient client = new SvnClient())
+            {
+                Console.WriteLine("ログ取得を開始します");
+                // SVN リポジトリのパス
+                string path = _URI;
+                Uri uri = null;
+                try
+                {
+                    uri = new Uri(path);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+               
+                SvnUriTarget repos = new SvnUriTarget(uri);
+                SvnLogArgs logArgs = new SvnLogArgs()
+                {
+                    Limit = _Limit,
+                };
+
+                Collection<SvnLogEventArgs> logs;
+                client.GetLog(uri, logArgs, out logs);
+
+                Encoding enc = Encoding.GetEncoding("shift_jis");
+                string outputPath = @"result.txt";
+                using (StreamWriter writer = new StreamWriter(outputPath, true))
+                {
+                    foreach (var log in logs)
+                    {
+                        string rev = log.Revision.ToString();
+                        string message = log.LogMessage.Replace("\r\n", " ").Trim();
+                        int pos = message.LastIndexOf('#');
+                        string id = (pos > 0) ? message.Substring(pos) : ""; 
+                        DateTime time = log.Time;
+
+                        writer.WriteLine($"{rev}	{time.ToString("yyyy/MM/dd")}	{id}	{message}");
+                    }
+                }
+            }
+        }
+
+        private void TestCheckout()
         {
             using (SvnClient client = new SvnClient())
             {
@@ -20,7 +102,8 @@ namespace svn_client
 
 
                 SvnInfoEventArgs serverInfo;
-                if (! client.GetInfo(repos, out serverInfo)) {
+                if (!client.GetInfo(repos, out serverInfo))
+                {
                     Console.WriteLine("error");
                 }
 
@@ -31,7 +114,7 @@ namespace svn_client
                 //{
                 //    Console.WriteLine("error");
                 //}
-                
+
 
                 ////処理結果の表示  
                 //foreach (var node in list)
@@ -55,7 +138,6 @@ namespace svn_client
                 string filename = "";
                 // 配列でもOK
                 client.Update(path + filename, updateconfig);
-
 
             }
         }
